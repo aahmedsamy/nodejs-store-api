c64d04f4c6a79b916525a001320f0c38f8a0f0d0
Ahmed <aahmedsamy.as@gmail.com>
2023-09-06 21:18:17 +0300

    added pagination functionality

--- controllers/products.js
+++ controllers/products.js
@@ -23,12 +23,16 @@ const getAllProducts = async (req, res) => {
     } else {
         result = result.sort('createdAt')
     }
-    if(fields){
+    if (fields) {
         const fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList)
     }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
     const products = await result
-    res.status(200).json({products, nbHits: products.length})
+    res.status(200).json({nbHits: products.length, products})
 }
 
 module.exports = {
